fade64754cab55210809df465308caf36f44972b
AlaaEldin123 <alaaeldinalhallak@gmail.com>
2023-03-01 02:20:21 +0400

    Add Vendor Register Page Setup Part 8

--- Multi_Vendor_Ecommerce/resources/views/admin/body/sidebar.blade.php
+++ Multi_Vendor_Ecommerce/resources/views/admin/body/sidebar.blade.php
@@ -1,3 +1,15 @@
+@php
+	$id = Auth::user()->id;
+	$verdorId = App\Models\User::find($id);
+	$status = $verdorId->status; 
+@endphp
+
+
+
+
+
+
+
 <div class="sidebar-wrapper" data-simplebar="true">
     <div class="sidebar-header">
         <div>
@@ -84,6 +96,50 @@
     </li>
 
  
+    @if($status === 'active')
+
+    <li>
+        <a href="javascript:;" class="has-arrow">
+            <div class="parent-icon"><i class='bx bx-home-circle'></i>
+            </div>
+            <div class="menu-title">Product Manage </div>
+        </a>
+        <ul>
+            <li> <a href="index.html"><i class="bx bx-right-arrow-alt"></i>All Product</a>
+            </li>
+            <li> <a href="dashboard-eCommerce.html"><i class="bx bx-right-arrow-alt"></i>Add Product</a>
+            </li>
+
+        </ul>
+    </li>
+    <li>
+        <a href="javascript:;" class="has-arrow">
+            <div class="parent-icon"><i class="bx bx-category"></i>
+            </div>
+            <div class="menu-title">All Order</div>
+        </a>
+        <ul>
+            <li> <a href="app-emailbox.html"><i class="bx bx-right-arrow-alt"></i>Email</a>
+            </li>
+            <li> <a href="app-chat-box.html"><i class="bx bx-right-arrow-alt"></i>Chat Box</a>
+            </li>
+
+        </ul>
+    </li>
+
+
+
+@else
+
+@endif		
+
+
+
+
+
+
+
+
 
 
 
