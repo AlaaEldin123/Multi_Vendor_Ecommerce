5e7092a2d5ed1599eceb76762b16b69b31424bbd
AlaaEldin123 <alaaeldinalhallak@gmail.com>
2023-02-28 23:37:23 +0400

    Subcategory Crud Part 1

--- Multi_Vendor_Ecommerce/routes/web.php
+++ Multi_Vendor_Ecommerce/routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\VendorController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Backend\SubCategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -124,7 +124,16 @@ Route::middleware(['auth','role:admin'])->group(function() {
     
     });
 
+//Sub Category All Route 
+Route::controller(SubCategoryController::class)->group(function(){
+    Route::get('/all/subcategory' , 'AllSubCategory')->name('all.subcategory');
+    Route::get('/add/category' , 'AddCategory')->name('add.category');
+    Route::post('/store/category' , 'StoreCategory')->name('store.category');
+    Route::get('/edit/category/{id}' , 'EditCategory')->name('edit.category');
+    Route::post('/update/category' , 'UpdateCategory')->name('update.category');
+    Route::get('/delete/category/{id}' , 'DeleteCategory')->name('delete.category');
 
+});
 
 
 }); // End Middleware
