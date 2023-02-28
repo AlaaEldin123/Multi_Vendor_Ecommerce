ace00a78449967d2eeec6ee4a83ed66b700f629d
AlaaEldin123 <alaaeldinalhallak@gmail.com>
2023-03-01 00:49:48 +0400

    Add Vendor Register Page Setup Part 5

--- Multi_Vendor_Ecommerce/resources/views/backend/vendor/inactive_vendor.blade.php
+++ Multi_Vendor_Ecommerce/resources/views/backend/vendor/inactive_vendor.blade.php
@@ -104,9 +104,7 @@
 
 				<td>
 
-<a href="{{ route('edit.subcategory',$item->id) }}" class="btn btn-info">Vendor Details</a>
-
-
+                    <a href="{{ route('inactive.vendor.details',$item->id) }}" class="btn btn-info">Vendor Details</a>
 
 
 
