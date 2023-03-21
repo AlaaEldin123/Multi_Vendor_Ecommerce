99a56ee67cff601789cd982f9f462d3456e12f6c
AlaaEldin123 <alaaeldinalhallak@gmail.com>
2023-03-22 02:54:03 +0400

    Product Details Show Part 2

--- Multi_Vendor_Ecommerce/app/Http/Controllers/Frontend/IndexController.php
+++ Multi_Vendor_Ecommerce/app/Http/Controllers/Frontend/IndexController.php
@@ -16,7 +16,20 @@ class IndexController extends Controller
     public function ProductDetails($id,$slug){
 
         $product = Product::findOrFail($id);
-        return view('frontend.product.product_details',compact('product'));
 
-     } // End Method 
+
+
+        $color = $product->product_color;
+        $product_color = explode(',', $color);
+
+        $size = $product->product_size;
+        $product_size = explode(',', $size);
+
+
+        return view('frontend.product.product_details',compact('product','product_color','product_size'));
+
+
+    } // End Method 
+
+
 }
