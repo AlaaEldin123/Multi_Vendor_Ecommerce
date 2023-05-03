1e476b405f62673fe18623bf31a32e88c26efad2
AlaaEldin123 <alaaeldinalhallak@gmail.com>
2023-05-03 05:25:39 +0400

    Product View Modal With Ajax Part 4

--- Multi_Vendor_Ecommerce/resources/views/frontend/master_dashboard.blade.php
+++ Multi_Vendor_Ecommerce/resources/views/frontend/master_dashboard.blade.php
@@ -10,23 +10,23 @@
     <meta property="og:title" content="" />
     <meta property="og:type" content="" />
     <meta property="og:url" content="" />
-    <meta name="csrf-token" content="{{ csrf-token() }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta property="og:image" content="" />
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('frontend/assets/imgs/theme/favicon.svg')}}" />
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('frontend/assets/imgs/theme/favicon.svg') }}" />
     <!-- Template CSS -->
-    <link rel="stylesheet" href="{{asset('frontend/assets/css/plugins/animate.min.css')}}" />
-    <link rel="stylesheet" href="{{asset('frontend/assets/css/main.css?v=5.3')}}" />
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/plugins/animate.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('frontend/assets/css/main.css?v=5.3') }}" />
 </head>
 
 <body>
     <!-- Modal -->
- 
+
     <!-- Quick view -->
- @include('frontend.body.quickview')
+    @include('frontend.body.quickview')
     <!-- Header  -->
- @include('frontend.body.header')
-   <!-- End Header  -->
+    @include('frontend.body.header')
+    <!-- End Header  -->
 
 
 
@@ -41,9 +41,9 @@
 
 
     <main class="main">
-     
 
-         @yield('main')
+
+        @yield('main')
 
     </main>
 
@@ -58,57 +58,71 @@
         <div class="preloader d-flex align-items-center justify-content-center">
             <div class="preloader-inner position-relative">
                 <div class="text-center">
-                    <img src="{{asset('frontend/assets/imgs/theme/loading.gif')}}" alt="" />
+                    <img src="{{ asset('frontend/assets/imgs/theme/loading.gif') }}" alt="" />
                 </div>
             </div>
         </div>
     </div>
     <!-- Vendor JS-->
-    <script src="{{asset('frontend/assets/js/vendor/modernizr-3.6.0.min.js')}}"></script>
-    <script src="{{asset('frontend/assets/js/vendor/jquery-3.6.0.min.js')}}"></script>
-    <script src="{{asset('frontend/assets/js/vendor/jquery-migrate-3.3.0.min.js')}}"></script>
-    <script src="{{asset('frontend/assets/js/vendor/bootstrap.bundle.min.js')}}"></script>
-    <script src="{{asset('frontend/assets/js/plugins/slick.js')}}"></script>
-    <script src="{{asset('frontend/assets/js/plugins/jquery.syotimer.min.js')}}"></script>
-    <script src="{{asset('frontend/assets/js/plugins/waypoints.js')}}"></script>
-    <script src="{{asset('frontend/assets/js/plugins/wow.js')}}"></script>
-    <script src="{{asset('frontend/assets/js/plugins/perfect-scrollbar.js')}}"></script>
-    <script src="{{asset('frontend/assets/js/plugins/magnific-popup.js')}}"></script>
-    <script src="{{asset('frontend/assets/js/plugins/select2.min.js')}}"></script>
-    <script src="{{asset('frontend/assets/js/plugins/counterup.js')}}"></script>
-    <script src="{{asset('frontend/assets/js/plugins/jquery.countdown.min.js')}}"></script>
-    <script src="{{asset('frontend/assets/js/plugins/images-loaded.js')}}"></script>
-    <script src="{{asset('frontend/assets/js/plugins/isotope.js')}}"></script>
-    <script src="{{asset('frontend/assets/js/plugins/scrollup.js')}}"></script>
-    <script src="{{asset('frontend/assets/js/plugins/jquery.vticker-min.js')}}"></script>
-    <script src="{{asset('frontend/assets/js/plugins/jquery.theia.sticky.js')}}"></script>
-    <script src="{{asset('frontend/assets/js/plugins/jquery.elevatezoom.js')}}"></script>
+    <script src="{{ asset('frontend/assets/js/vendor/modernizr-3.6.0.min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/vendor/jquery-3.6.0.min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/vendor/jquery-migrate-3.3.0.min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/vendor/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/slick.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/jquery.syotimer.min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/waypoints.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/wow.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/perfect-scrollbar.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/magnific-popup.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/select2.min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/counterup.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/jquery.countdown.min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/images-loaded.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/isotope.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/scrollup.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/jquery.vticker-min.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/jquery.theia.sticky.js') }}"></script>
+    <script src="{{ asset('frontend/assets/js/plugins/jquery.elevatezoom.js') }}"></script>
     <!-- Template  JS -->
-    <script src="{{asset('frontend/assets/js/main.js')}}?v=5.3"></script>
-    <script src="{{asset('frontend/assets/js/shop.js')}}?v=5.3"></script>
+    <script src="{{ asset('frontend/assets/js/main.js') }}?v=5.3"></script>
+    <script src="{{ asset('frontend/assets/js/shop.js') }}?v=5.3"></script>
 
 
     <script type="text/javascript">
-    
         $.ajaxSetup({
-            headers:{
-                'X-CSRF-TOKEN':$('meta[name="csrf-token"]').attr('centent')
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('centent')
             }
         })
         /// Start product view with Modal 
-        function productView(id){
-        // alert(id)
-        $.ajax({
-            type: 'GET',
-            url: '/product/view/modal/'+id,
-            dataType: 'json',
-            success:function(data){
-                console.log(data)
-            }
-        })
-    }
-        
-     </script>
+        function productView(id) {
+            // alert(id)
+            $.ajax({
+                type: 'GET',
+                url: '/product/view/modal/' + id,
+                dataType: 'json',
+                success: function(data) {
+                    // console.log(data)
+                    $('#pname').text(data.product.product_name);
+                    $('#pprice').text(data.product.selling_price);
+                    $('#pcode').text(data.product.product_code);
+                    $('#pcategory').text(data.product.category.category_name);
+                    $('#pbrand').text(data.product.brand.brand_name);
+                    $('#pimage').attr('src', '/' + data.product.product_thambnail);
+
+                    // Product Price 
+                    if (data.product.discount_price == null) {
+                        $('#pprice').text('');
+                        $('#oldprice').text('');
+                        $('#pprice').text(data.product.selling_price);
+                    } else {
+                        $('#pprice').text(data.product.discount_price);
+                        $('#oldprice').text(data.product.selling_price);
+                    } // end else
+                }
+            })
+        }
+    </script>
 
 </body>
 
