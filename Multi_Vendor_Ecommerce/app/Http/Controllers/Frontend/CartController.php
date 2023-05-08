5a27fc89631a6378a46f5db0ea2a571473843a36
AlaaEldin123 <alaaeldinalhallak@gmail.com>
2023-05-08 10:43:30 +0400

    fix Cartpage Read All Cart Product With Ajax

--- Multi_Vendor_Ecommerce/app/Http/Controllers/Frontend/CartController.php
+++ Multi_Vendor_Ecommerce/app/Http/Controllers/Frontend/CartController.php
@@ -144,7 +144,11 @@ class CartController extends Controller
     }// End Method
 
 
+    public function CartRemove($rowId){
+        Cart::remove($rowId);
+        return response()->json(['success' => 'Successfully Remove From Cart']);
 
+    }// End Method
 
 
 
--- Multi_Vendor_Ecommerce/resources/views/frontend/mycart/view_cart.blade.php
+++ Multi_Vendor_Ecommerce/resources/views/frontend/mycart/view_cart.blade.php
@@ -37,54 +37,7 @@
                                     <th scope="col" class="end">Remove</th>
                                 </tr>
                             </thead>
-                            <tbody>
-
-
-        <tr class="pt-30">
-            <td class="custome-checkbox pl-30">
-
-            </td>
-            <td class="image product-thumbnail pt-40"><img src="assets/imgs/shop/product-1-1.jpg" alt="#"></td>
-            <td class="product-des product-name">
-                <h6 class="mb-5"><a class="product-name mb-10 text-heading" href="shop-product-right.html">Field Roast Chao Cheese Creamy Original</a></h6>
-                <div class="product-rate-cover">
-                    <div class="product-rate d-inline-block">
-                        <div class="product-rating" style="width:90%">
-                        </div>
-                    </div>
-                    <span class="font-small ml-5 text-muted"> (4.0)</span>
-                </div>
-            </td>
-            <td class="price" data-title="Price">
-                <h4 class="text-body">$2.51 </h4>
-            </td>
-
-              <td class="price" data-title="Price">
-                <h6 class="text-body">Color </h6>
-            </td>
-
-              <td class="price" data-title="Price">
-                <h6 class="text-body">Size </h6>
-            </td>
-            <td class="text-center detail-info" data-title="Stock">
-                <div class="detail-extralink mr-15">
-                    <div class="detail-qty border radius">
-                        <a href="#" class="qty-down"><i class="fi-rs-angle-small-down"></i></a>
-                        <input type="text" name="quantity" class="qty-val" value="1" min="1">
-                        <a href="#" class="qty-up"><i class="fi-rs-angle-small-up"></i></a>
-                    </div>
-                </div>
-            </td>
-            <td class="price" data-title="Price">
-                <h4 class="text-brand">$2.51 </h4>
-            </td>
-            <td class="action text-center" data-title="Remove"><a href="#" class="text-body"><i class="fi-rs-trash"></i></a></td>
-        </tr>
-
-
-
-
-                            </tbody>
+                            <tbody id="cartPage">
                         </table>
                     </div>
 
@@ -113,7 +66,43 @@
                             <div class="border p-md-4 cart-totals ml-30">
                         <div class="table-responsive">
                             <table class="table no-border">
-                                <tbody id="cartPage">
+                                <tbody>
+                                    <tr>
+                                        <td class="cart_total_label">
+                                            <h6 class="text-muted">Subtotal</h6>
+                                        </td>
+                                        <td class="cart_total_amount">
+                                            <h4 class="text-brand text-end">$12.31</h4>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td scope="col" colspan="2">
+                                            <div class="divider-2 mt-10 mb-10"></div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="cart_total_label">
+                                            <h6 class="text-muted">Shipping</h6>
+                                        </td>
+                                        <td class="cart_total_amount">
+                                            <h5 class="text-heading text-end">Free</h4</td> </tr> <tr>
+                                        <td class="cart_total_label">
+                                            <h6 class="text-muted">Estimate for</h6>
+                                        </td>
+                                        <td class="cart_total_amount">
+                                            <h5 class="text-heading text-end">United Kingdom</h4</td> </tr> <tr>
+                                        <td scope="col" colspan="2">
+                                            <div class="divider-2 mt-10 mb-10"></div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="cart_total_label">
+                                            <h6 class="text-muted">Total</h6>
+                                        </td>
+                                        <td class="cart_total_amount">
+                                            <h4 class="text-brand text-end">$12.31</h4>
+                                        </td>
+                                    </tr>
                                 </tbody>
                             </table>
                         </div>
