5118ec13d9b78a357c7262d1c3762dd2ac384d1c
AlaaEldin123 <alaaeldinalhallak@gmail.com>
2023-05-08 10:49:44 +0400

    Cart Page Cart Quantity Decrement With Ajax

--- Multi_Vendor_Ecommerce/app/Http/Controllers/Frontend/CartController.php
+++ Multi_Vendor_Ecommerce/app/Http/Controllers/Frontend/CartController.php
@@ -152,7 +152,14 @@ class CartController extends Controller
 
 
 
+    public function CartDecrement($rowId){
 
+        $row = Cart::get($rowId);
+        Cart::update($rowId, $row->qty -1);
+
+        return response()->json('Decrement');
+
+    }// End Method
 
 
 
