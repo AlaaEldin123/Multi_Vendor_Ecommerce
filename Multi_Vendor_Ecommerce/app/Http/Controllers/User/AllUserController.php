5dd7364ca6ac0ee3100c0eca8a396d39a5cf4c48
AlaaEldin123 <alaaeldinalhallak@gmail.com>
2023-05-11 01:51:51 +0400

    Read Orders In User Profile

--- Multi_Vendor_Ecommerce/app/Http/Controllers/User/AllUserController.php
+++ Multi_Vendor_Ecommerce/app/Http/Controllers/User/AllUserController.php
@@ -7,22 +7,26 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use App\Models\Order;
 class AllUserController extends Controller
 {
-    public function UserAccount(){
+    public function UserAccount()
+    {
         $id = Auth::user()->id;
         $userData = User::find($id);
-        return view('frontend.userdashboard.account_details',compact('userData'));
-
+        return view('frontend.userdashboard.account_details', compact('userData'));
     } // End Method
 
-    public function UserChangePassword(){
-        return view('frontend.userdashboard.user_change_password' );
-   } // End Method 
+    public function UserChangePassword()
+    {
+        return view('frontend.userdashboard.user_change_password');
+    } // End Method 
 
 
-   public function UserOrderPage(){
-         return view('frontend.userdashboard.user_order_page');
-   }// End Method 
-   
+    public function UserOrderPage(){
+        $id = Auth::user()->id;
+        $orders = Order::where('user_id',$id)->orderBy('id','DESC')->get();
+          return view('frontend.userdashboard.user_order_page',compact('orders'));
+    }// End Method 
+
 }
