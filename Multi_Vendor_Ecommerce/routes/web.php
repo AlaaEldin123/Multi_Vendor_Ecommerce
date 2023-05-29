732060ad330045067987c0cfaf798dc7886df523
AlaaEldin123 <alaaeldinalhallak@gmail.com>
2023-05-29 10:57:20 +0400

    Admin Panel ¦ Read All Register Users

--- Multi_Vendor_Ecommerce/routes/web.php
+++ Multi_Vendor_Ecommerce/routes/web.php
@@ -26,6 +26,18 @@ use App\Http\Controllers\User\StripeController;
 use App\Http\Controllers\Backend\VendorOrderController;
 use App\Http\Controllers\Backend\ReportController;
 use App\Http\Controllers\User\AllUserController;
+use App\Http\Controllers\Backend\ActiveUserController;
+
+
+
+
+
+
+
+
+
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -320,6 +332,12 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
 
         Route::get('/order/by/user', 'OrderByUser')->name('order.by.user');
         Route::post('/search/by/user', 'SearchByUser')->name('search-by-user');
+    });
+    // Active user and vendor All Route 
+    Route::controller(ActiveUserController::class)->group(function () {
+
+        Route::get('/all/user', 'AllUser')->name('all-user');
+        Route::get('/all/vendor', 'AllVendor')->name('all-vendor');
     });
 });
 
