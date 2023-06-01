a483a5a0cfec3c7bb0c9534ab081e0961910ac65
AlaaEldin123 <alaaeldinalhallak@gmail.com>
2023-06-01 10:10:15 +0400

    Order Tracking By Invoice Part 2

---
+++ Multi_Vendor_Ecommerce/resources/views/frontend/traking/track_order.blade.php
@@ -0,0 +1,31 @@
+@extends('frontend.master_dashboard')
+
+@section('main')
+
+
+
+
+@section('title')
+
+   Order Tracking Page 
+
+@endsection
+
+
+
+
+
+
+
+<h1>order Tracking </h1>
+
+
+
+
+
+
+
+
+
+
+@endsection
