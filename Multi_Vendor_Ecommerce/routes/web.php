509cb6929fcd4fad2cd092d634e586584debd8a3
AlaaEldin123 <alaaeldinalhallak@gmail.com>
2023-06-04 12:17:36 +0400

    Multi Admin Setup Part 3

--- Multi_Vendor_Ecommerce/routes/web.php
+++ Multi_Vendor_Ecommerce/routes/web.php
@@ -433,7 +433,10 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
 
         Route::get('/all/admin', 'AllAdmin')->name('all.admin');
         Route::get('/add/admin', 'AddAdmin')->name('add.admin');
+        Route::post('/admin/user/store' , 'AdminUserStore')->name('admin.user.store');
     });
+
+    
 });
 
 
